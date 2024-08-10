7d04992e2613e8a201bb7fd2f1c5da64298e1db6
alexburnet <alexburnet03@gmail.com>
2024-08-10 17:34:12 +0100

    Boiler plate for initial portfolio website

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // JavaScript code for future enhancements
 // Currently, no functionality is required
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
